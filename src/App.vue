9266b5fee2bbd499508086c66808d0145f29cc87
kevin <boogerlad@gmail.com>
2019-02-25 05:16:40 -0500

    mvp

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<label>
+			type name of new board and hit enter
+			<input v-on:keyup.enter="boards.push({name: $event.target.value, categories: []})"/>
+		</label>
+		<draggable v-model="boards">
+		<div class="board" v-for="board in boards">
+			<label>
+				edit board name
+				<input v-model="board.name"/>
+			</label>
+			<br>
+			<label>
+				type name of new category in {{ board.name }}
+				<input v-on:keyup.enter="board.categories.push({name: $event.target.value, cards: []})"/>
+				<div class="container">
+					<draggable v-model="board.categories">
+					<div v-for="category in board.categories">
+						<label>
+							edit category name
+							<input v-model="category.name">
+						</label>
+						<br>
+						<label>
+							type name of new card in {{ category.name }}
+							<input v-on:keyup.enter="category.cards.push({name: $event.target.value})"/>
+						</label>
+						<draggable v-model="category.cards">
+						<div v-for="card in category.cards" v-on:dblclick="category.cards.splice(category.cards.findIndex(element => element.name === card.name), 1)">
+							{{ card.name }}
+						</div>
+						</draggable>
+					</div>
+					</draggable>
+				</div>
+			</label>
+		</div>
+		</draggable>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import draggable from 'vuedraggable'
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+	name: 'app',
+	components: {
+		draggable
+	},
+	data: function() {
+		return {
+			boards: []
+		}
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container > div {
+	display: flex;
+}
+.container > div > div {
+	border-style: dotted;
+}
+.board {
+	border-style: groove;
 }
 </style>
